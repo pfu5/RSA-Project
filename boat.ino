9a7e74b6745ad149514fea1785dfc28c65f809fb
jimwang418 <95256753+jimwang418@users.noreply.github.com>
2021-12-01 19:00:52 -0500

    Add files via upload
    Update boat and remote control codes

--- boat.ino
+++ boat.ino
@@ -5,23 +5,24 @@
 
 // define the communication pin numbers
 #define INT_PIN 2
-#define RST_PIN 9
-#define CS_PIN 10
+#define RST_PIN 6
+#define CS_PIN 5
 
 // define the servomotors' pins
-#define ACCEL_PIN 7
-#define DIREC_PIN 8
+#define ACCEL_PIN 10
+#define DIREC_PIN 9
 
 // declare all variables
-Servo accel;
-Servo direc;
+const int FREQUENCY = 910;
+Servo accelServo;
+Servo direcServo;
 
 RH_RF69 transceiver(CS_PIN, INT_PIN); // initialize the transceiver object
 
 void setup() {
   Serial.begin(9600); // set the serial monitor communication rate
-  accel.attach(ACCEL_PIN);
-  direc.attach(DIREC_PIN);
+  accelServo.attach(ACCEL_PIN);
+  direcServo.attach(DIREC_PIN);
   pinMode(RST_PIN, OUTPUT); // set the RST pin as output
   digitalWrite(RST_PIN, HIGH); // set the RST pin to high
   delay(10); // wait for 10ms
@@ -30,24 +31,35 @@ void setup() {
   if (!transceiver.init()) { // make sure the init is successful
     Serial.println("init failed");
   }
-  if (!transceiver.setFrequency(915.0)) { // make sure the setFrequency is successful
+  if (!transceiver.setFrequency(FREQUENCY)) { // make sure the setFrequency is successful
     Serial.println("setFrequency failed");
   }
   transceiver.setTxPower(14, true); // set transceiver's power level
+  for(int throttle = 0; throttle < 180; throttle++) {
+    accelServo.write(throttle);
+    delay(5);
+  }
+  for(int throttle = 180; throttle > 0; throttle--) {
+    accelServo.write(throttle);
+    delay(5);
+  }
 }
 
 void loop() {
   if(transceiver.available()) {
-    uint8_t packet[RH_RF69_MAX_MESSAGE_LEN];
+    uint8_t packet[4];
     uint8_t len = sizeof(packet);
     if(transceiver.recv(packet, &len)) {
       if(((bool)packet[2]) == false) {
-        accel.write(0);
-        direc.write(90);
+        accelServo.write(0);
+        direcServo.write(90);
         return;
       }
       if(((bool)packet[3]) == true) {
-        
+        double convertedAccel = ((double)packet[0])/255*180;
+        double convertedDirec = ((double)packet[1])/255*180;
+        accelServo.write((int)convertedAccel);
+        direcServo.write((int)convertedDirec);
         return;
       }
       // TODO put autonomous code right here
--- remote_control.ino
+++ remote_control.ino
@@ -4,28 +4,29 @@
 
 // define the communication pin numbers
 #define INT_PIN 2
-#define RST_PIN 9
-#define CS_PIN 10
+#define RST_PIN 6
+#define CS_PIN 5
 
 // define the joysticks' pins
-#define ON_OFF_PIN 3
+#define ON_OFF_PIN 10
 #define MODE_PIN 7
-#define ACCEL_PIN A1
-#define DIREC_PIN A2
+#define ACCEL_PIN A2
+#define DIREC_PIN A3
 
 // declare all variables
+const int FREQUENCY = 910;
 bool on_off_state;
 bool manual_state;
-unsigned long prev_time;
+// unsigned long prev_time;
 
 RH_RF69 transceiver(CS_PIN, INT_PIN); // initialize the transceiver object
 
 void setup() {
   Serial.begin(9600); // set the serial monitor communication rate
   pinMode(ON_OFF_PIN, INPUT_PULLUP);
-  attachInterrupt(1, on_off_switch, FALLING);
+  // attachInterrupt(1, on_off_switch, FALLING);
   on_off_state = false;
-  prev_time = millis();
+  // prev_time = millis();
   manual_state = true;
   pinMode(MODE_PIN, INPUT_PULLUP);
   pinMode(ACCEL_PIN, INPUT);
@@ -38,7 +39,7 @@ void setup() {
   if(!transceiver.init()) { // make sure the init is successful
     Serial.println("init failed");
   }
-  if(!transceiver.setFrequency(915.0)) { // make sure the setFrequency is successful
+  if(!transceiver.setFrequency(FREQUENCY)) { // make sure the setFrequency is successful
     Serial.println("setFrequency failed");
   }
   transceiver.setTxPower(14, true); // set transceiver's power level
@@ -48,8 +49,13 @@ void loop() {
   uint8_t packet[4];
   int rawAccelReading = analogRead(ACCEL_PIN);
   int rawDirecReading = analogRead(DIREC_PIN);
-  float convertedAccelReading = ((float)rawAccelReading)/1023*255;
-  float convertedDirecReading = ((float)rawDirecReading)/1023*255;
+  double convertedAccelReading = (((double)rawAccelReading)/1023-0.5)*2*255;
+  double convertedDirecReading = ((double)rawDirecReading)/1023*255;
+  convertedAccelReading = constrain(convertedAccelReading, 0, 255);
+  convertedDirecReading = constrain(convertedDirecReading, 0, 255);
+  if (digitalRead(ON_OFF_PIN) == LOW) {
+    on_off_state = !on_off_state;
+  }
   if (digitalRead(MODE_PIN) == LOW) {
     manual_state = !manual_state;
   }
@@ -57,14 +63,7 @@ void loop() {
   packet[1] = (uint8_t)convertedDirecReading;
   packet[2] = (uint8_t)on_off_state;
   packet[3] = (uint8_t)manual_state;
-  transceiver.send(packet, strlen(packet));
+  transceiver.send(packet, sizeof(packet));
   transceiver.waitPacketSent();
-  delay(200);
-}
-
-void on_off_switch() {
-  if(millis() - prev_time > 200) {
-    on_off_state = !on_off_state;
-    prev_time = millis();
-  }
+  delay(50);
 }
