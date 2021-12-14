833998c83229f9d0cb4353154872c175803cbedc
Junxiang Wang <jwang334@jh.edu>
2021-12-13 20:22:57 -0500

    Add comments and change in GPS code

--- boat.ino
+++ boat.ino
@@ -18,134 +18,147 @@
 const int serialpinRX = 4; //connects to board TX
 const int serialpinTX = 3; //connects to beard RX
 
+// define the pin for collision detection
 #define BUMPER_PIN 8
 
 // declare all variables
-const int FREQUENCY = 905;
-Servo accelServo;
-Servo direcServo;
-unsigned long first_hit;
-bool manual_state;
+const int FREQUENCY = 905; // frequency to use for transceiver connection
+Servo accelServo; // Servo object for throttle
+Servo direcServo; // Servo object for rudder
+unsigned long first_hit; // record time of first collision
+bool manual_state; // keep track of the manual/autonomous state of the boat
 
-// for GPS
 bool newData = false; //controls data recording
-//initializes instance
-TinyGPS myGPS;
-SoftwareSerial ss(serialpinRX, serialpinTX);
-//initializes lattitude and longtitude
-float initialFlat = 0.0;
-float initialFlon = 0.0;
-float initialAge = 0.0;
-//initializes variables for distance and angle
-float circleRadius = 50.0;
-bool initialized = false;
+TinyGPS myGPS; // declare GPS instance object
+SoftwareSerial ss(serialpinRX, serialpinTX); // start software serial communication
+float initialFlat, initialFlon; //declare variables for stored origin latitude and longtitude
+float maxRadius = 50.0; // define the maximum allowable radius
+bool initialized = false; // keep track whether autonomous mode is initialized
 
 RH_RF69 transceiver(CS_PIN, INT_PIN); // initialize the transceiver object
 
 void setup() {
   Serial.begin(9600); // set the serial monitor communication rate
-  ss.begin(9600);
-  delay(1000);
+  ss.begin(9600); // set the software serial communication rate
+  delay(1000); // wait for one second
+  // attach the servomotor pins to the Servo objects
   accelServo.attach(ACCEL_PIN);
   direcServo.attach(DIREC_PIN);
-  direcServo.write(90);
-  manual_state = true;
-  first_hit = millis();
-  pinMode(BUMPER_PIN, INPUT_PULLUP);
+  direcServo.write(90); // set rudder to the middle
+  manual_state = true; // default to begin with manual control
+  first_hit = millis(); // initialize the time-keeping variable
+  pinMode(BUMPER_PIN, INPUT_PULLUP); // set the pin for collision detection as input pullup
+  // manually reset the transceiver
   pinMode(RST_PIN, OUTPUT); // set the RST pin as output
   digitalWrite(RST_PIN, HIGH); // set the RST pin to high
   delay(10); // wait for 10ms
   digitalWrite(RST_PIN, LOW); // set the RST pin to low
   delay(10); // wait for 10ms
-  if (!transceiver.init()) { // make sure the init is successful
+  if (!transceiver.init()) { // make sure the initialization is successful
     Serial.println("init failed");
   }
   if (!transceiver.setFrequency(FREQUENCY)) { // make sure the setFrequency is successful
     Serial.println("setFrequency failed");
   }
   transceiver.setTxPower(14, true); // set transceiver's power level
-  for (int throttle = 0; throttle < 180; throttle++) {
+  // manually initialize the throttle servomotor
+  for (int throttle = 0; throttle < 180; throttle++) { // go from zero to full speed
     accelServo.write(throttle);
     delay(5);
   }
-  for (int throttle = 180; throttle > 0; throttle--) {
+  for (int throttle = 180; throttle > 0; throttle--) { // back down from full speed to zero
     accelServo.write(throttle);
     delay(5);
   }
 }
 
 void loop() {
-  if (transceiver.available()) {
-    uint8_t packet[4];
-    uint8_t len = sizeof(packet);
-    if (transceiver.recv(packet, &len)) {
-      if (((bool)packet[2]) == false) {
+  if (transceiver.available()) { // checks for available transceiver data
+    uint8_t packet[4]; // declare the data packet to receive
+    uint8_t len = sizeof(packet); // get the length of the packet
+    if (transceiver.recv(packet, &len)) { // receive the data packet
+      if (((bool)packet[2]) == false) { // do nothing if received an "off" state
         accelServo.write(0);
         direcServo.write(90);
-        initialized = false;
+        initialized = false; // this is always false when not in automous
         return;
       }
-      if (((bool)packet[3]) == true) {
+      if (((bool)packet[3]) == true) { // switch manual/autonomous states if commanded to
         manual_state = !manual_state;
       }
-      if (manual_state) {
+      if (manual_state) { // if in manual mode
+        // scale the received throttle and rudder values down to a range from 0 to 180
         double convertedAccel = ((double)packet[0]) / 255 * 180;
         double convertedDirec = ((double)packet[1]) / 255 * 180;
+        // write to the servomotors the converted values
         accelServo.write((int)convertedAccel);
         direcServo.write((int)convertedDirec);
-        initialized = false;
+        initialized = false; // this is always false when not in autonomous
         return;
       }
+      // switch to manual if time between two collisions is less than 10 seconds (with debouncing)
       if (digitalRead(BUMPER_PIN) == LOW && millis() - first_hit > 500) {
         if (millis() - first_hit < 10000) {
           manual_state = true;
         }
         first_hit = millis();
       }
-      newData = false;
-      if (ss.available()) {
-        char c = ss.read();
-        if (myGPS.encode(c)) {
-          newData = true;
+      float flat, flon; // declare latitude and longtitude variables
+      unsigned long age; // declare angle variable
+      unsigned long startTime = millis(); // record time
+      bool newData = false; // keep track whether there is good GPS data
+      while (1<2) {
+        if (ss.available()) {  // if there is serial data available...
+          byte c = ss.read(); // read a byte of serial data
+          if ( gps.encode(c) ) {  // if a GPS sentence is finished
+            gps.f_get_position(&flat, &flon, &age); // get the position and store the information in the variables
+            Serial.println("GPS encoded.");
+            break;
+          }
+          else if ( (millis() - startTime) > 2000 ) { // or give up after 2s without valid GPS data
+            Serial.println("No GPS data.");
+            break;
+          }
         }
       }
-      if (newData) {
-        float flat, flon;
-        unsigned long age;
-        myGPS.f_get_position(&flat, &flon, &age);
-        if (!initialized) {
+      if (newData) { // if there is good data
+        if (!initialized) { // if not initialized--only happens once when first entering autonomous from another state
+          // store the current latitude and longtitude as the origin for reference
           initialFlat = flat;
           initialFlon = flon;
-          initialAge = age;
           initialized = true;
         }
-        //records latitudinal and longtitudinal distance
+        // record latitudinal and longtitudinal distance from the current position to the origin
         float distanceMeterLat = TinyGPS::distance_between(flat, 0, initialFlat, 0);
         float distanceMeterLon = TinyGPS::distance_between(0, flon, 0, initialFlon);
+        // correct for sign for both cases
         if (flat < initialFlat) {
-          distanceMeterLat = -1. * distanceMeterLat;//corrects for sign
+          distanceMeterLat = -1. * distanceMeterLat;
         }
         if (flon < initialFlon) {
-          distanceMeterLon = -1. * distanceMeterLon;//corrects for sign
+          distanceMeterLon = -1. * distanceMeterLon;
         }
-        //calculates distance from original postion
+        //calculate and print radial distance from origin
         float radialDistance = sqrt(distanceMeterLat * distanceMeterLat + distanceMeterLon * distanceMeterLon);
-        //prints distance and angle
         Serial.println(radialDistance);
-        if (radialDistance < circleRadius) {
+        if (radialDistance < maxRadius) { // keep going straight if within a certain distance from the origin
           accelServo.write(180);
           direcServo.write(90);
         }
-        else {
+        else { // otherwise, travel at an angle (until back within the specified distance)
           accelServo.write(90);
           direcServo.write(45);
           Serial.println("out of circle");
         }
       }
-      else {
+      else { // do nothing if no GPS data
         accelServo.write(0);
         direcServo.write(90);
       }
     }
+  }
+  else { // do nothing if no transceiver data
+    accelServo.write(0);
+    direcServo.write(90);
   }
 }
--- remote_control.ino
+++ remote_control.ino
@@ -2,40 +2,44 @@
 #include <SPI.h>
 #include <RH_RF69.h>
 
-// define the communication pin numbers
+// define the communication pins
 #define INT_PIN 2
 #define RST_PIN 6
 #define CS_PIN 5
 
-// define the joysticks' pins
+// define the joystick's pins
 #define ON_OFF_PIN 10
-#define MODE_PIN 7
 #define ACCEL_PIN A2
 #define DIREC_PIN A3
 
+// define the pin for mode switch
+#define MODE_PIN 7
+
 // declare all variables
-const int FREQUENCY = 905;
-bool on_off_state;
-unsigned long prev_on_off;
-unsigned long prev_manual;
+const int FREQUENCY = 905; // frequency to use for transceiver connection
+bool on_off_state; // keep track of the on/off state of the boat
+unsigned long prev_on_off; // for debouncing of on/off button
+unsigned long prev_manual; // for debouncing of mode switch button
 
 RH_RF69 transceiver(CS_PIN, INT_PIN); // initialize the transceiver object
 
 void setup() {
   Serial.begin(9600); // set the serial monitor communication rate
-  pinMode(ON_OFF_PIN, INPUT_PULLUP);
-  on_off_state = false;
+  on_off_state = false; // initialze the state as false (boat off)
+  // initialize debouncing variables
   prev_on_off = millis();
   prev_manual = millis();
-  pinMode(MODE_PIN, INPUT_PULLUP);
-  pinMode(ACCEL_PIN, INPUT);
-  pinMode(DIREC_PIN, INPUT);
+  pinMode(ON_OFF_PIN, INPUT_PULLUP); // set the on/off button to input pullup
+  pinMode(MODE_PIN, INPUT_PULLUP); // set the mode switch button to input pullup
+  pinMode(ACCEL_PIN, INPUT); // set the joystick throttle direction as input
+  pinMode(DIREC_PIN, INPUT); // set the joystick rudder direction
   pinMode(RST_PIN, OUTPUT); // set the RST pin as output
+  // manually reset the transceiver
   digitalWrite(RST_PIN, HIGH); // set the RST pin to high
   delay(10); // wait for 10ms
   digitalWrite(RST_PIN, LOW); // set the RST pin to low
   delay(10); // wait for 10ms
-  if (!transceiver.init()) { // make sure the init is successful
+  if (!transceiver.init()) { // make sure the initialization is successful
     Serial.println("init failed");
   }
   if (!transceiver.setFrequency(FREQUENCY)) { // make sure the setFrequency is successful
@@ -45,27 +49,34 @@ void setup() {
 }
 
 void loop() {
-
-  uint8_t packet[4];
+  uint8_t packet[4]; // initialize data packet array
+  // obtain the raw analog readings from both joystick directions
   int rawAccelReading = analogRead(ACCEL_PIN);
   int rawDirecReading = analogRead(DIREC_PIN);
-  double convertedAccelReading = (((double)rawAccelReading) / 1023 - 0.5) * 2 * 255;
+  // convert these readings to a range with maximum of 255 for maximum precision in a single byte
+  double convertedAccelReading = (((double)rawAccelReading) / 1023 - 0.5) * 2 * 255; // only keeps one direction (positive)
   double convertedDirecReading = ((double)rawDirecReading) / 1023 * 255;
+  // constrain these converted readings to between 0 and 255 for sanity
   convertedAccelReading = constrain(convertedAccelReading, 0, 255);
   convertedDirecReading = constrain(convertedDirecReading, 0, 255);
+  // switch the on/off state if the on/off button is pressed (with debouncing)
   if (digitalRead(ON_OFF_PIN) == LOW && millis() - prev_on_off > 500) {
     on_off_state = !on_off_state;
-    prev_on_off = millis();
+    prev_on_off = millis(); // reset the time-keeping variable
   }
-  bool man_aut_switch = false;
+  bool man_aut_switch = false; // variable used to send a mode switch command to the boat; default to false
+  // set the mode switch variable to true if the mode switch button is pressed (with debouncing)
   if (digitalRead(MODE_PIN) == LOW && millis() - prev_manual > 500) {
     man_aut_switch = true;
-    prev_manual = millis();
+    prev_manual = millis(); // reset the time-keeping variable
   }
+  // construct the data packet to be sent as the following sequence:
+  // throttle value to be set, rudder value to be set, on/off state, mode switch (or not)
   packet[0] = (uint8_t)convertedAccelReading;
   packet[1] = (uint8_t)convertedDirecReading;
   packet[2] = (uint8_t)on_off_state;
   packet[3] = (uint8_t)man_aut_switch;
+  // send the data packet, wait for packet sent, and delay
   transceiver.send(packet, sizeof(packet));
   transceiver.waitPacketSent();
   delay(50);
